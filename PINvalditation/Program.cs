571c1f37e440fd317b5be92844bee85804c7e094
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:49:15 +0300

    whileloop

---
+++ PINvalditation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PINvalditation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            int i = 0;
+
+            while (i < 3)
+               
+            {
+                Console.WriteLine("sisesta PIN");
+                int pin = Convert.ToInt32(Console.ReadLine());
+                
+                if (pin == 1234)
+                {
+                    Console.WriteLine("tere tulemast");
+                    break;
+                }
+                else if (i < 3)                         
+                {
+                    Console.WriteLine("proovi uuesti");
+                    i = i + 1;                    
+                }
+
+
+            }
+        }
+    }
+}
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt PIN-Koodi
+            //programm võrdleb sisestatud PIN-Koodi arvuga 1234
+            //kui sisestatud PIN-Kood on 1234
+            //programm kuvab "tere tulemast!"
+            //kui sisestatud PIN on vale, programm kuvab konsoolis
+            //"vale pin. Proovi uuesti"
+            //katsete arv on piiramatu
+
+            bool loopActive = true;
+           
+            while (loopActive)
+            {
+                Console.WriteLine("sisesta PIN");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("terve tuloa");
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Vale pin proovi uuesti");
+                    i = i + 1
+                }
+            }
+            
+            
+                
+        
+        }
+
+    }
+}
